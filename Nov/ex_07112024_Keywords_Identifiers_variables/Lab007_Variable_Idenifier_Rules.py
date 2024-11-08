f34e1a724a4ce67f124ff868b6a0ef0aa6b586ac
Gajendran10 <gajendran.mani10@gmail.com>
2024-11-08 14:13:35 +0530

    Initial commit

---
+++ Nov/ex_07112024_Keywords_Identifiers_variables/Lab007_Variable_Idenifier_Rules.py
@@ -0,0 +1,35 @@
+#_age=45
+_= 12
+# variable name--> identifier-->
+#= --> assignment operator
+#variable value --> 12
+
+print(_)
+
+
+a= 20
+_ = 45
+_ =_+1 # 45+1
+
+print(_)
+
+abc123=78
+#123abc
+_gajen="welcome"
+_abc=23
+#&123 = 23
+
+pi = 3.14
+name = "gajen"
+isMale = True
+
+print(type(pi))
+print(type(isMale))
+print(type(name))
+
+#Complex umbers - Python - iota
+# i - root to per i
+
+complex_number= 2+3j
+print(complex_number.real)
+print(complex_number.imag)
